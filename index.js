2a3b7cdf288135aa0463d91384aa692a0f40c300
dann <dillydot22@gmail.com>
2022-06-17 10:41:35 +0100

    welcome {user} issue

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function getName() {
-	let playerName = document.getElementById("myForm").elements[0].value;
-	if (playerName.length !== 0) {
-		console.log(playerName);
-		alert(playerName);
-		// window.open("./index.html");
-		window.location.replace("http://127.0.0.1:5500/home.html");
-	} else {
-		alert("Input a username to continue");
-	}
-}
+(function () {
+  //check if DOM elements are loaded
+  document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('myForm');
+
+    document.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      let user = document.querySelector("#name").value
+      console.log(user);
+      alert(user);
+      window.localStorage.setItem('name', user);
+      // window.open("./index.html");
+      window.location.replace("http://127.0.0.1:5500/home.html");
+    });
+  }, false);
+})();
